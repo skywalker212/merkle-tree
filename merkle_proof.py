d7abedae44e8df61f6bcbef2d2e91aea4d1e3516
Akash Gajjar <akashgajjar8@gmail.com>
2018-11-26 14:32:40 +0530

    implement functions in merkle_proof.py

--- merkle_proof.py
+++ merkle_proof.py
@@ -1,26 +1,32 @@
+"""Akash Gajjar - 201501212@daiict.ac.in"""
+
 from utils import *
-import math
 from node import Node
 
 
 def merkle_proof(tx, merkle_tree):
-    """Given a tx and a Merkle tree object, retrieve its list of tx's and
-    parse through it to arrive at the minimum amount of information required
-    to arrive at the correct block header. This does not include the tx
-    itself.
-
-    Return this data as a list; remember that order matters!
-    """
-    #### YOUR CODE HERE
-
-
+    proof = []
+    node = merkle_tree._root
+    i = merkle_tree.leaves.index(tx)
+    low = 0
+    high = len(merkle_tree.leaves)
+    while high - low > 1:
+        mid = (low + high) // 2
+        if i < mid:
+            proof.append(Node("r", node._right.data if type(node._right) != str else node._right))
+            node = node._left
+            high =  mid
+        else:
+            proof.append(Node("l", node._left.data if type(node._left) != str else node._left))
+            node = node._right
+            low = mid
+    return proof
 
 def verify_proof(tx, merkle_proof):
-    """Given a Merkle proof - constructed via `merkle_proof(...)` - verify
-    that the correct block header can be retrieved by properly hashing the tx
-    along with every other piece of data in the proof in the correct order
-    """
-    #### YOUR CODE HERE
-
-
-    
+    proof = [tx] + merkle_proof[::-1]
+    while len(proof) > 1:
+        if proof[1].direction == 'l':
+            proof.insert(0, hash_data(proof.pop(1).tx + proof.pop(0)))
+        else:
+            proof.insert(0, hash_data(proof.pop(0) + proof.pop(0).tx))
+    return proof[0]
